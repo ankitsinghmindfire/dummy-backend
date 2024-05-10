47f5411c71daacc2aaec08162cc7395411f72fb5
ankitsinghmindfire <168187880+ankitsinghmindfire@users.noreply.github.com>
2024-05-10 17:04:57 +0530

    Created data.js

---
+++ apis/data.js
@@ -0,0 +1,306 @@
+const data = {
+data: [
+    {
+      id: 0,
+      series: "Lannister",
+      description: "Cersei Lorem Ipsum is simply dummy text of the printing and typesetting industry.",
+      numberOfVersions: 31,
+      lastActionDate: "22/10/2022"
+    },
+    {
+      id: 1,
+      series: "Snow",
+      description: "Jon Lorem Ipsum is simply dummy text of the printing and typesetting industry.",
+      numberOfVersions: 14,
+      lastActionDate: "22/10/2022"
+    },
+    {
+      id: 2,
+      series: "Lannister",
+      description: "Jaime Lorem Ipsum is simply dummy text of the printing and typesetting industry.,",
+      numberOfVersions: 31,
+      lastActionDate: "22/10/2022"
+    },
+    {
+      id: 3,
+      series: "Stark",
+      description: "Arya Lorem Ipsum is simply dummy text of the printing and typesetting industry.",
+      numberOfVersions: 11,
+      lastActionDate: "22/10/2022"
+    },
+    {
+      id: 4,
+      series: "Targaryen",
+      description: "Daener Lorem Ipsum is simply dummy text of the printing and typesetting industry.ys",
+      numberOfVersions: 31,
+      lastActionDate: "22/10/2022"
+    },
+    {
+      id: 5,
+      series: "Melisandre",
+      description: null,
+      lastActionDate: "22/10/2022"
+    },
+    {
+      id: 6,
+      series: "Clifford",
+      description: "Ferrar Lorem Ipsum is simply dummy text of the printing and typesetting industry.a",
+      numberOfVersions: 44,
+      lastActionDate: "22/10/2022"
+    },
+    {
+      id: 7,
+      series: "Frances",
+      description: "Rossin Lorem Ipsum is simply dummy text of the printing and typesetting industry.i",
+      numberOfVersions: 36,
+      lastActionDate: "22/10/2022"
+    },
+    {
+      id: 8,
+      series: "Roxie",
+      description: "Harvey Lorem Ipsum is simply dummy text of the printing and typesetting industry.",
+      numberOfVersions: 65,
+      lastActionDate: "22/10/2022"
+    }
+  ],
+  rows: [
+    [
+      {
+        id: 3,
+        status: "Approved",
+        approvedDate: "12/01/2292",
+        nextAction: "24/12/2024",
+        approvedBy: "Joe Smith",
+        pendingTasks: "To be fixed and tested"
+      },
+      {
+        id: 2,
+        status: "Rejected",
+        approvedDate: "12/01/2292",
+        nextAction: "24/12/2024",
+        approvedBy: "Test name",
+        pendingTasks: "To be fixed and tested"
+      },
+      {
+        id: 1,
+        status: "Rejected",
+        approvedDate: "12/01/2292",
+        nextAction: "24/12/2024",
+        approvedBy: "New name",
+        pendingTasks: "To be fixed and tested"
+      }
+    ],
+    [
+      {
+        id: 3,
+        status: "Approved",
+        approvedDate: "12/01/2292",
+        nextAction: "24/12/2024",
+        approvedBy: "Test Datana",
+        pendingTasks: "To be fixed and tested"
+      },
+      {
+        id: 2,
+        status: "Rejected",
+        approvedDate: "12/01/2292",
+        nextAction: "24/12/2024",
+        approvedBy: "Test Datana",
+        pendingTasks: "To be fixed and tested"
+      },
+      {
+        id: 1,
+        status: "Rejected",
+        approvedDate: "12/01/2292",
+        nextAction: "24/12/2024",
+        approvedBy: "New Data am",
+        pendingTasks: "To be fixed and tested"
+      }
+    ],
+    [
+      {
+        id: 3,
+        status: "Approved",
+        approvedDate: "12/01/2292",
+        nextAction: "24/12/2024",
+        approvedBy: "New Dummy",
+        pendingTasks: "To be fixed and tested"
+      },
+      {
+        id: 2,
+        status: "Rejected",
+        approvedDate: "12/01/2292",
+        nextAction: "24/12/2024",
+        approvedBy: "New Dummy",
+        pendingTasks: "To be fixed and tested"
+      },
+      {
+        id: 1,
+        status: "Rejected",
+        approvedDate: "12/01/2292",
+        nextAction: "24/12/2024",
+        approvedBy: "New Dummy",
+        pendingTasks: "To be fixed and tested"
+      }
+    ],
+    [
+      {
+        id: 3,
+        status: "Approved",
+        approvedDate: "12/01/2292",
+        nextAction: "24/12/2024",
+        approvedBy: "Dummy nam",
+        pendingTasks: "To be fixed and tested"
+      },
+      {
+        id: 2,
+        status: "Rejected",
+        approvedDate: "12/01/2292",
+        nextAction: "24/12/2024",
+        approvedBy: "Dummy nam",
+        pendingTasks: "To be fixed and tested"
+      },
+      {
+        id: 1,
+        status: "Rejected",
+        approvedDate: "12/01/2292",
+        nextAction: "24/12/2024",
+        approvedBy: "Dummy nam",
+        pendingTasks: "To be fixed and tested"
+      }
+    ],
+    [
+      {
+        id: 3,
+        status: "Approved",
+        approvedDate: "12/01/2292",
+        nextAction: "24/12/2024",
+        approvedBy: "Joe Smith",
+        pendingTasks: "To be fixed and tested"
+      },
+      {
+        id: 2,
+        status: "Rejected",
+        approvedDate: "12/01/2292",
+        nextAction: "24/12/2024",
+        approvedBy: "Joe Smith",
+        pendingTasks: "To be fixed and tested"
+      },
+      {
+        id: 1,
+        status: "Rejected",
+        approvedDate: "12/01/2292",
+        nextAction: "24/12/2024",
+        approvedBy: "Joe Smith",
+        pendingTasks: "To be fixed and tested"
+      }
+    ],
+    [
+      {
+        id: 3,
+        status: "Approved",
+        approvedDate: "12/01/2292",
+        nextAction: "24/12/2024",
+        approvedBy: "Give name",
+        pendingTasks: "To be fixed and tested"
+      },
+      {
+        id: 2,
+        status: "Rejected",
+        approvedDate: "12/01/2292",
+        nextAction: "24/12/2024",
+        approvedBy: "Give name",
+        pendingTasks: "To be fixed and tested"
+      },
+      {
+        id: 1,
+        status: "Rejected",
+        approvedDate: "12/01/2292",
+        nextAction: "24/12/2024",
+        approvedBy: "Give name",
+        pendingTasks: "To be fixed and tested"
+      }
+    ],
+    [
+      {
+        id: 3,
+        status: "Approved",
+        approvedDate: "12/01/2292",
+        nextAction: "24/12/2024",
+        approvedBy: "John Doe",
+        pendingTasks: "To be fixed and tested"
+      },
+      {
+        id: 2,
+        status: "Rejected",
+        approvedDate: "12/01/2292",
+        nextAction: "24/12/2024",
+        approvedBy: "John Doe",
+        pendingTasks: "To be fixed and tested"
+      },
+      {
+        id: 1,
+        status: "Rejected",
+        approvedDate: "12/01/2292",
+        nextAction: "24/12/2024",
+        approvedBy: "John Doe",
+        pendingTasks: "To be fixed and tested"
+      }
+    ],
+    [
+      {
+        id: 3,
+        status: "Approved",
+        approvedDate: "12/01/2292",
+        nextAction: "24/12/2024",
+        approvedBy: "Any Name",
+        pendingTasks: "To be fixed and tested"
+      },
+      {
+        id: 2,
+        status: "Rejected",
+        approvedDate: "12/01/2292",
+        nextAction: "24/12/2024",
+        approvedBy: "Any Name",
+        pendingTasks: "To be fixed and tested"
+      },
+      {
+        id: 1,
+        status: "Rejected",
+        approvedDate: "12/01/2292",
+        nextAction: "24/12/2024",
+        approvedBy: "Any Name",
+        pendingTasks: "To be fixed and tested"
+      }
+    ],
+    [
+      {
+        id: 3,
+        status: "Approved",
+        approvedDate: "12/01/2292",
+        nextAction: "24/12/2024",
+        approvedBy: "No New Nae",
+        pendingTasks: "To be fixed and tested"
+      },
+      {
+        id: 2,
+        status: "Rejected",
+        approvedDate: "12/01/2292",
+        nextAction: "24/12/2024",
+        approvedBy: "No New Nae",
+        pendingTasks: "To be fixed and tested"
+      },
+      {
+        id: 1,
+        status: "Rejected",
+        approvedDate: "12/01/2292",
+        nextAction: "24/12/2024",
+        approvedBy: "No New Nae",
+        pendingTasks: "To be fixed and tested"
+      }
+    ]
+  ]
+};
+
+export default function handler(req, res) {
+  res.status(200).json(data);
+}
